fa1bde5d46d4996631aeec510428174b6aaf16f6
Peter Elmers <peter.elmers@yahoo.com>
2013-06-13 05:00:06 -0500

    small bugfix to 56

--- euler56.cpp
+++ euler56.cpp
@@ -5,8 +5,8 @@ int main(int, char**);
 int main(int argc, char** argv) {
     int sum, maxsum = 0;
     mpz_class bignum;
-    for (int a = 1; a < 101; ++a) {
-        for (int b = 1; b < 101; ++b) {
+    for (int a = 1; a < 100; ++a) {
+        for (int b = 1; b < 100; ++b) {
             mpz_ui_pow_ui(bignum.get_mpz_t(), a, b);
             sum = digit_sum(bignum);
             if (sum > maxsum)
