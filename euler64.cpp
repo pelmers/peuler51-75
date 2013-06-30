acddde72da1bd427ab6c944e274cba73e82689b9
Peter Elmers <peter.elmers@yahoo.com>
2013-06-30 10:23:49 -0500

    problem 64 shortened

--- euler64.cpp
+++ euler64.cpp
@@ -1,17 +1,12 @@
 #include "euler.hpp"
 
-using std::floor;
-
 int main(void);
 
 int main(void) {
     int c = 0;
-    vector<int> t;
-    for (int i = 2; i <= 10000; ++i) {
-        t = find_repeated_sqrt(i);
-        if ((t.size()-1) % 2 != 0)
+    for (int i = 2; i <= 10000; ++i)
+        if ((find_repeated_sqrt(i).size()-1) % 2 != 0)
             ++c;
-    }
     cout << c << " odd period sqrts up to 10000\n";
 }
 
